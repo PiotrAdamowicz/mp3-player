478929ce4536f29655fe57369d934aad55923462
Piotr Adamowicz <adamowicz.piotr2@gmail.com>
2026-07-24 18:45:49 +0200

    refactor: :recycle: ts setup

---
+++ src/config.ts
@@ -0,0 +1,9 @@
+export const MUSIC_DIR = "/home/piotrek/mp3-player/music";
+export const LOG_DIR = "/home/piotrek/mp3-player/logs";
+export const WEB_UI_DIR = "/home/piotrek/mp3-player/web-ui";
+
+export const HTTP_HOST = "0.0.0.0";
+export const HTTP_PORT = 8080;
+
+export const AUDIO_OUTPUT = "alsa";
+export const BLUEALSA_DEVICE = "bluealsa";
