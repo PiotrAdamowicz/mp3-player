063687582eac4be2fcf63e35dd47f33f1fcf33fd
Piotr Adamowicz <adamowicz.piotr2@gmail.com>
2026-08-01 18:30:04 +0200

    feat: :sparkles: styling playlist loading and error elements

--- web-ui/src/componets/playlist/Playlist.tsx
+++ web-ui/src/componets/playlist/Playlist.tsx
@@ -9,25 +9,32 @@ const Playlist: FunctionComponent<PlaylistProps> = () => {
     const { data, isPending, isError, error } = usePlaylistQuery();
 
     if (isPending) {
-        return <div>Loading playlist...</div>;
+        return (
+            <div className="w-full text-center">
+                <span className="loading loading-spinner loading-xl my-50 bg-primary"></span>
+            </div>)
     }
 
     if (isError) {
-        return <div>Playlist error: {(error as Error).message}</div>;
+        return (
+            <div className="w-full text-center">
+                <div className="my-50 text-error text-xl">Playlist error: {(error as Error).message}</div>
+            </div>
+        )
     }
-    console.log({ data })
+
     const tracks = data?.tracks ?? [];
-    console.log({ tracks })
+
 
     return (
-        <section>
-            <h3>Library Tracks</h3>
-            <p>{tracks.length} songs</p>
+        <section className="mx-auto flex justify-center flex-col max-w-2xl text-center gap-4">
+            <h3 className="my-2 text-4xl font-bold">Playlist Name</h3>
+            <p className="my-1 text-2xl font-semibold">{tracks.length} songs</p>
 
-            <ul>
+            <ul className="list bg-base-100 rounded-box shadow-md">
                 {tracks.map((track, index) => (
-                    <li key={`${track.filename}-${index}`}>
-                        <div>{track.title || track.filename}</div>
+                    <li className="text-start my-2 text-xl flex" key={`${track.filename}-${index}`}>
+                        <div>{track.title || track.filename}</div><span className="mx-2"> - </span>
                         <div>{track.artist || "Unknown Artist"}</div>
                     </li>
                 ))}
